8a381524be8f50f7a4c2254c46ae5f4cc4decbc3
Pedro Santos <pedrosantoscrlx@gmail.com>
2026-04-12 18:13:23 +0100

    refactor: session2 exercise Factory Pattern

--- session2/designPatterns.py
+++ session2/designPatterns.py
@@ -8,20 +8,48 @@
 #     Create concrete classes for Circle and Square.
 #     Write a factory function to return the correct object.
 
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """Abstraction Shape class"""
+    def __init__(self,name: str) -> str:
+        return name
+        
+    @abstractmethod
+    def describe(self) -> str:
+        pass
+    
+class Circle(Shape):
+    """Concrete classes for Circle"""
+    def describe(self) -> str:
+        return self.name
+    
+class Square:
+    """Concrete classes for Square"""
+    def describe(self) -> str:
+        return self.name
 
 class FactoryShape:
     """Creation of circle and square shapes"""
+    factory_stock = {}
     
-    def __init__(self, name: str):
-        self.name = name
-
-    def create_circle(name: str) -> str:
-        return f"{name} circle was created"
+    def create_circle(self,name: str) -> str:
+        shape_type = "Circle"
+        FactoryShape.factory_stock.setdefault(name, shape_type)
+        return f"{name} circle was created type:{shape_type}"
     
-    def create_square(name: str) -> str:
-        return f"{name} square was created"
+    def create_square(self,name: str) -> str:
+        shape_type = "Square"
+        FactoryShape.factory_stock.setdefault(name, shape_type)
+        return f"{name} square was created type: {shape_type}"
+    
+factory = FactoryShape()
 
-circle = FactoryShape.create_circle("Pedro Santos")
-square = FactoryShape.create_square("Jorge Alves")
+circle = factory.create_circle("Pedro Redondo")
+square = factory.create_square("Pedro Quadrado")
 
-print(f"{circle} and {square}")
+print(circle.describe)
+
+# keys = FactoryShape.factory_stock.keys()
+# values = FactoryShape.factory_stock.values()
+# print(f"We got created already: {keys} and their types are: {values}")
